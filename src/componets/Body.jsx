f474a64a73358d8ce7cdff88c524ff087d8415f1
IKeagu Mark <ikeagumark@gmail.com>
2025-09-06 16:08:57 +0100

    my message

--- src/componets/Body.jsx
+++ src/componets/Body.jsx
@@ -5,7 +5,7 @@ export default function Body(){
         <section className="p-20">
             <p className="font-bold p-6 text-6xl w-103">Make remote work</p>
             <p className=" p-6 w-100">get your team in sync,no matter your location.Streamline processes,creat team ritual,and</p>
-            <button className="bg-black mt-2xl border rounded w-25 h-14  text-white hover:underline">Learn more </button>
+            <button className="bg-black mt-2xl border Rounded w-39 h-14  text-white hover:underline">Learn more </button>
         </section>
                 <section className="ml-40 h-100 w-90">
                     <img src="image-hero-desktop.png" alt="" />
